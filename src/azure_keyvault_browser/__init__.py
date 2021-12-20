e2c258512f7d3869bf5dbb0d7dcb0addd0f1eae1
Sam Dobson <1309834+samdobson@users.noreply.github.com>
2021-12-20 15:37:29 +0000

    Add support for py3.7 and py3.8

--- src/azure_keyvault_browser/__init__.py
+++ src/azure_keyvault_browser/__init__.py
@@ -1,7 +1,14 @@
-import importlib.metadata
 import sys
 
-if (sys.version_info[0], sys.version_info[1]) < (3, 9):
-    sys.exit("keyvault-browser requires Python 3.9 or later.")
+if (sys.version_info[0], sys.version_info[1]) < (3, 7):
+    sys.exit("azurecr-browser requires Python 3.7 or later.")
 
-__version__ = importlib.metadata.version("azure_keyvault_browser")
+try:
+    import importlib.metadata
+
+    __version__ = importlib.metadata.version("azure_keyvault_browser")
+except ModuleNotFoundError:
+    import importlib_metadata
+
+    __version__ = importlib_metadata.version("azure_keyvault_browser")
+
